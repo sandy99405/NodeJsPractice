640a4f3651348b9956be5c4ca416dc146b85c743
sandy99405 <sandysandilya99@gmail.com>
2023-08-31 22:09:52 +0530

    New Commit

--- ThirdPractice.js
+++ ThirdPractice.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 
 http.createServer(function(req,res){
      fs.readFile("DemoFile.html", function(err,data){
-          res.writeHead(200,'Content-type':'text/html');
+          res.writeHead(200,{'Content-type':'text/html'});
           res.write(data);
           return res.end();
      });}
--- urlPractice.js
+++ urlPractice.js
@@ -1,4 +1,4 @@
-var http = require('http')
+/* var http = require('http')
 var url = require('url')
 
 
@@ -9,3 +9,60 @@ http.createServer(function (req,res){
        res.write(text);
        res.end();
 }).listen(3000)
+
+
+
+
+
+
+
+var http = require('http')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-type':'text/html'});
+        res.end('Hello world')
+}).listen(3000)
+
+
+
+//// Module.js
+
+var myDateTime = function(){
+       return Date();
+}
+
+
+/// Main.js
+
+var http = require('https')
+
+var dt = require('./Module')
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-type':'text/html'})
+     res.write("Today's date is " + dt.myDateTime())
+     res.end()
+}).listen(3000)
+
+ */
+
+//// Third code practice
+var http = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-type':'text/html'})
+     const q = url.parse(req.url,true).query
+     var text = q.month + " " + q.year
+     res.write(text)
+     res.end()
+}).listen(3000)
+
+
+
+
+
+
+
+
