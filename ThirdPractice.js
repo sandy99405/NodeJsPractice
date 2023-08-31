3ae8c351519a5732b7151119e90607dcd7a8ca49
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2023-08-31 22:08:17 +0530

    Create ThirdPractice.js

---
+++ ThirdPractice.js
@@ -0,0 +1,12 @@
+var http = require('http')
+
+var fs = require('fs')
+
+http.createServer(function(req,res){
+     fs.readFile("DemoFile.html", function(err,data){
+          res.writeHead(200,'Content-type':'text/html');
+          res.write(data);
+          return res.end();
+     });}
+  ).listen(3000);
+            
