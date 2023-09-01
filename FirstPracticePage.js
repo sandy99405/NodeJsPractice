1386c1fa724c96415d23bbfd2cd0bfe7b29d04b2
sandy99405 <sandysandilya99@gmail.com>
2023-09-01 07:57:50 +0530

    New Commit

--- FirstPracticePage.js
+++ FirstPracticePage.js
@@ -100,6 +100,19 @@ http.createServer(function (req,res){
         res.end("Hello world")
 }).listen(3000)
 
+var http = require('http')
+
+var url = require('url')
+
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-type':'text/html'})
+     res.write(req.url)
+     res.end()
+}).listen(3000)
+
+
+
 
 
 
