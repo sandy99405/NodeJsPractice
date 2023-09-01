ecef264aa8b85cbec180a59ae04ebee13d3a9ff3
sandy99405 <sandysandilya99@gmail.com>
2023-09-01 07:58:41 +0530

    Commited again

--- FirstPracticePage.js
+++ FirstPracticePage.js
@@ -106,10 +106,10 @@ var url = require('url')
 
 
 http.createServer(function(req,res){
-     res.writeHead(200,{'Content-type':'text/html'})
-     res.write(req.url)
-     res.end()
-}).listen(3000)
+     res.writeHead(200,{'Content-type':'text/html'});
+     res.write(req.url);
+     res.end();
+}).listen(3000);
 
 
 
