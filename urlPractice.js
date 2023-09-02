334b0787d3a3878e0ecc19831df9986cf2b119c6
sandy99405 <sandysandilya99@gmail.com>
2023-09-02 08:29:46 +0530

    Again another change

--- urlPractice.js
+++ urlPractice.js
@@ -8,10 +8,22 @@ http.createServer(function(req,res){
  */
 
 
-var http = require('http')
+/* var http = require('http')
 
 http.createServer(function(req,res){
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write(req.url)
         res.end()
-}).listen(8000);
+}).listen(8000); */
+
+
+var http = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-Type':'text/html'})
+     var q = url.parse(req.url,true).query 
+     var text = q.month + " " + q.year
+     res.end(text)     
+}).listen(8000)
