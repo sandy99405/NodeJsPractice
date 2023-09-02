eba65bc534473fe7f81618a782470ea05a166374
sandy99405 <sandysandilya99@gmail.com>
2023-09-02 08:41:05 +0530

    commit made again

--- urlPractice.js
+++ urlPractice.js
@@ -16,7 +16,7 @@ http.createServer(function(req,res){
         res.end()
 }).listen(8000); */
 
-
+/* 
 var http = require('http')
 
 var url = require('url')
@@ -26,4 +26,17 @@ http.createServer(function(req,res){
      var q = url.parse(req.url,true).query 
      var text = q.month + " " + q.year
      res.end(text)     
+}).listen(8000) */
+
+var http = require('http')
+
+var fs = require('fs')
+
+
+http.createServer(function(req,res){
+      fs.readFile('DemoFile.html',function(err,data){
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write(data)
+            return res.end()
+      })
 }).listen(8000)
