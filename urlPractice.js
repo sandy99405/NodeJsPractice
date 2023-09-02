dd3af9e8e9ac62c3026ecb3cc017183902a31bef
sandy99405 <sandysandilya99@gmail.com>
2023-09-02 08:19:27 +0530

    New Commit again

--- urlPractice.js
+++ urlPractice.js
@@ -1,8 +1,17 @@
-var http = require('http')
+/* var http = require('http')
 
 http.createServer(function(req,res){
        res.writeHead(200,{'Content-type':'text/html'})
        res.write('Hello World')
        res.end()
 }).listen(3000)
+ */
 
+
+var http = require('http')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(req.url)
+        res.end()
+}).listen(8000);
