daaa959ba47b83bd721d020bada8273e5c9a5055
sandy99405 <sandysandilya99@gmail.com>
2023-09-04 22:02:45 +0530

    node Url practice

--- urlPractice.js
+++ urlPractice.js
@@ -17,7 +17,7 @@ http.createServer(function(req,res){
       res.end()
 }).listen(3000) */
 
-
+/* 
 var http = require('http')
 
 var url = require('url')
@@ -28,4 +28,17 @@ http.createServer(function(req,res){
      var text = q.month() + " " + q.year();
      res.write(text)
      res.end()
-}).listen(3000)
+}).listen(3000) */
+
+
+var http = require('http')
+
+var fs = require('fs')
+
+http.createServer(function(req,res){
+      fs.readFile("DemoFile.html",function(err,data){
+           res.writeHead(200,{'Content-type':'text/html'})
+           res.write(data)
+           return res.end()
+      })
+}).listen(3001)
