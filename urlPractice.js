1b3969f86080173baa0bbafcfb157b1fead28d86
sandy99405 <sandysandilya99@gmail.com>
2023-09-04 21:53:54 +0530

    Commit for the day

--- urlPractice.js
+++ urlPractice.js
@@ -1,8 +1,31 @@
-var http = require('http')
+/* var http = require('http')
 
 http.createServer(function(req,res){
      res.writeHead(200,{'Content-type':'text/html'})
      res.write("Hello world")
      res.end()
-}).listen(3000)
+}).listen(3000) */
 
+
+/* var http  = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+      res.writeHead(200,{'Content-type':'text/html'})
+      res.write(req.url)
+      res.end()
+}).listen(3000) */
+
+
+var http = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-type':'text/html'});
+     var q = url.parse(req.url,true).query;
+     var text = q.month() + " " + q.year();
+     res.write(text)
+     res.end()
+}).listen(3000)
