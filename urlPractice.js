b902fca18e8d12851407139f0d4f91866ffd5299
sandy99405 <sandysandilya99@gmail.com>
2023-09-08 07:06:30 +0530

    node practice

--- urlPractice.js
+++ urlPractice.js
@@ -171,3 +171,10 @@ http.createServer(function (req, res) {
 
 // Practice today
 
+var http = require('http')
+
+http.createServer(function(req,res){
+     res.writeHead(200,{'Content-type':'text/html'})
+     res.write("Hello Sandy")
+     res.end()
+}).listen(8080)
