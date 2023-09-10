2dffb83d42f3fc9ada068221d6ca77257fe5d299
sandy99405 <sandysandilya99@gmail.com>
2023-09-10 09:34:42 +0530

    New changes made to the code recently

--- urlPractice.js
+++ urlPractice.js
@@ -18,7 +18,7 @@ var url = require('url')
 http.createServer(function(req,res){
        res.writeHead(200,{'Content-Type':'text/html'})
        var q = url.parse(req.url,true).query
-       var text = "Month is : "+q.month + "and year is : " + q.year
+       var text = "Month is : "+q.month + " and year is : " + q.year
        res.write(text)
        res.end()
 }).listen(3000)
