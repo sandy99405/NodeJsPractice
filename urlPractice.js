5a090e1f88500d97314a6d642237e19942720018
sandy99405 <sandysandilya99@gmail.com>
2023-09-10 09:33:00 +0530

    URL finding year and month practice done

--- urlPractice.js
+++ urlPractice.js
@@ -1,5 +1,5 @@
 // URL prcatice
-
+/* 
 var http = require('http')
 
 var url = require('url')
@@ -8,6 +8,17 @@ http.createServer(function(req,res){
        res.writeHead(200,{'Content-Type':'text/html'})
        res.write(url)
        res.end()
+}).listen(3000) */
+
+
+var http = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+       res.writeHead(200,{'Content-Type':'text/html'})
+       var q = url.parse(req.url,true).query
+       var text = "Month is : "+q.month + "and year is : " + q.year
+       res.write(text)
+       res.end()
 }).listen(3000)
-
-
