7e96a72f9ade5c5690730a3893f14492c442f4dd
sandy99405 <sandysandilya99@gmail.com>
2023-09-14 09:53:48 +0530

    third practice of node js done

--- urlPractice.js
+++ urlPractice.js
@@ -1,4 +1,4 @@
-var express = express()
+var express = require('express')
 
 var app = express()
 
@@ -16,4 +16,14 @@ http.createServer(function(req,res){
          res.writeHead(200,{'Content-type':'text/html'})
          res.write("This is the head");
          res.end()
+}).listen(2003)
+
+var url = require('url')
+
+http.createServer(function(req,res){
+      res.writeHead(200,{'Content-type':'text/html'})
+      var q = url.parse(req.url,true).query
+      var text = q.month +" " +q.year 
+      res.write(text)
+      res.end()
 }).listen(2003)
