a716c61670c18e03c5e0688ef57a160c54377ada
sandy99405 <sandysandilya99@gmail.com>
2023-09-16 10:44:16 +0530

    Node practice done

--- urlPractice.js
+++ urlPractice.js
@@ -2,28 +2,29 @@ var express = require('express')
 
 var app = express()
 
-app.get('/app/data',function(req,res){
-      const contentData = {
-           message : "This is a message from node js",
-           data : [1,2,3,4,5]
-      };
-      res.json(contentData)
-}).listen(2003)
+app.get('api/data',function(req,res){
+        const contentData = {
+              message : "Hai from urlPractice",
+              data : [1,2,3,4,5,6]
+        }
+        res.json(contentData);
+}).listen(3000)
+
 
 var http = require('http')
 
 http.createServer(function(req,res){
-         res.writeHead(200,{'Content-type':'text/html'})
-         res.write("This is the head");
-         res.end()
-}).listen(2003)
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write();
+        res.end();
+}).listen(3000)
 
 var url = require('url')
 
 http.createServer(function(req,res){
-      res.writeHead(200,{'Content-type':'text/html'})
-      var q = url.parse(req.url,true).query
-      var text = q.month +" " +q.year 
-      res.write(text)
-      res.end()
-}).listen(2003)
+         res.wtiteHead(200,{'Content-type':'text/html'});
+         var q = url.parse(req.url,true).query
+         var text = q.month + " " + q.year
+         res.write(text)
+         res.end()
+}).listen(3000)
