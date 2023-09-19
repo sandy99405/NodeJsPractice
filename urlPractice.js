f29bc03468b52b3e310dccfd8f4c75dd0762dd56
sandy99405 <sandysandilya99@gmail.com>
2023-09-19 08:39:40 +0530

    HTTP practice done

--- urlPractice.js
+++ urlPractice.js
@@ -11,3 +11,10 @@ app.use('/app/data',function(req,res){
 }).listen(3000) */
 
 
+var http = require('http')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write("Hello world")
+        res.end()
+}).listen(3000)
