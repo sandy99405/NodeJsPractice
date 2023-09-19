43416feb229f29f3ce0c08b05d637e9d24645cb0
sandy99405 <sandysandilya99@gmail.com>
2023-09-19 08:41:52 +0530

    Added the url practice

--- urlPractice.js
+++ urlPractice.js
@@ -11,10 +11,22 @@ app.use('/app/data',function(req,res){
 }).listen(3000) */
 
 
-var http = require('http')
+/* var http = require('http')
 
 http.createServer(function(req,res){
         res.writeHead(200,{'Content-type':'text/html'})
         res.write("Hello world")
         res.end()
+}).listen(3000) */
+
+
+var http = require('http')
+
+var url = require('url')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-type':'text/html'})
+        var q = url.parse(req.url,true).query
+        var text = q.month + " " + q.year
+        res.end(text)
 }).listen(3000)
