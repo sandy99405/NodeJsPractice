fa46351f7c893101d3f7b340486ede48331d011f
sandy99405 <sandysandilya99@gmail.com>
2023-09-21 08:12:15 +0530

    Yesterday's pending work

--- urlPractice.js
+++ urlPractice.js
@@ -1,32 +1,13 @@
-/* var express = require('express')
+// yesterday's commands practice 
 
-var app = express();
+var express = require('express')
+
+var app = express()
 
 app.use('/app/data',function(req,res){
-        const contentData = {
-                message : "A message sent from Node",
-                data : [1,2,3,4,5]
-        }
-        res.json(contentData)
-}).listen(3000) */
-
-
-/* var http = require('http')
-
-http.createServer(function(req,res){
-        res.writeHead(200,{'Content-type':'text/html'})
-        res.write("Hello world")
-        res.end()
-}).listen(3000) */
-
-
-var http = require('http')
-
-var url = require('url')
-
-http.createServer(function(req,res){
-        res.writeHead(200,{'Content-type':'text/html'})
-        var q = url.parse(req.url,true).query
-        var text = q.month + " " + q.year
-        res.end(text)
+       const contentData = {
+            message : "Hello from Node js",
+            data : [1,2,3,4,5]
+       }
+       res.json(contentData)
 }).listen(3000)
