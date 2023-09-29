480b2850671d4f47c937345a17108ede758b1dcd
sandy99405 <sandysandilya99@gmail.com>
2023-09-29 08:01:51 +0530

    express practice done

---
+++ urlPractice.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express()
+
+app.use('/app/data',function(req,res){
+        const contentData = {
+                message : "Hello from Node js",
+                data : [1,2,3,4,5]
+        }
+        res.json(contentData);
+}).listen(3000)
