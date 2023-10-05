3e86b3b9f98370b733e13750ad0463f4d07efabb
sandy99405 <sandysandilya99@gmail.com>
2023-10-05 06:34:49 +0530

    URL practice done

--- urlPractice.js
+++ urlPractice.js
@@ -17,3 +17,13 @@ http.createServer(function(req,res){
         res.write("Hello world")
 }).listen(3000)
 
+var url = require('url')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-type':'text/html'})
+        var q = url.parse(req.url,true).query 
+        var text = q.month + " " + q.year
+        res.write(text)
+        res.end()
+}).listen(3000)
+
