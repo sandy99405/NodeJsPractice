dc4a53a927915fd5309c82b1baa42333377440b7
sandy99405 <sandysandilya99@gmail.com>
2023-10-15 22:26:12 +0530

    HTTP done

--- urlPractice.js
+++ urlPractice.js
@@ -3,6 +3,16 @@ var express = require('expres')
 var ap = express()
 
 app.use(function(req,res){
+      const contentData = {
+            message : "Hello from Node js",
+            data : [1,2,3,4,5]
+      }
+      res.json(contentData)
+}).listen(3000)
+
+var http = require('http')
+
+http.createServer(function(req,res){
       res.writeHead(200,{'Content-type':'text/html'})
       res.write("Hello world")
       res.end()
