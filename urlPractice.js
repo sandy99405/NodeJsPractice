9a9eef0d5860d13b2b650f77cba28dd288f0cc70
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2023-10-31 13:32:01 +0530

    express done

--- urlPractice.js
+++ urlPractice.js
@@ -1,1 +1,11 @@
+var express = require('express')
 
+var app = express()
+
+app.use('app/data',function(req,res){
+           const contentData = {
+                    message : "Hello world",
+                    data : [1,2,3,4,5]
+           }
+           res.json(contentData)
+}).listen(3000)
