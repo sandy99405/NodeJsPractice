dca1ac6188847b9e7342bd60406215168a70f964
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2023-11-06 11:38:20 +0530

    File System

--- urlPractice.js
+++ urlPractice.js
@@ -30,3 +30,15 @@ http.createServer(function(req,res){
           res.end()
 }).listen(3000)
 
+
+
+var fs = require('fs')
+
+fs.writeFile("DemoFile.html",function(err,data){
+          http.createServer(function(req,res){
+                   res.writeHead(200,{'Content-type':'text/html')
+                   res.write(data)
+                   return res.end()
+          }).listen(3000)
+})
+
