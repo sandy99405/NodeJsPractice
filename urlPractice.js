f8f867620db165dd882b4e0924941400929ebca4
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2023-11-11 21:23:19 +0530

    Express done

---
+++ urlPractice.js
@@ -0,0 +1,12 @@
+var express = require('express')
+
+var app = express()
+
+app.use(function(res,res){
+        const contentData = {
+              message : "Hello from Node js",
+              data : [1,2,3,4,5]
+        }
+        res.json(contentData)
+}).listen(3000)
+
