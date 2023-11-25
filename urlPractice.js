fee226a4d83ab212f8dd5734ec9961359e8e1a15
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2023-11-25 17:08:45 +0530

    Express commit

--- urlPractice.js
+++ urlPractice.js
@@ -10,7 +10,7 @@ app.use("/app/data",function(req,res){
        res.json(contentData)
 }).listen(3000)
 
-var http = require('http')
+/* var http = require('http')
 
 http.createServer(function(req,res){
        res.writeHead(200,{'Content-type':'text/html'})
@@ -18,3 +18,14 @@ http.createServer(function(req,res){
        res.end()
 }).listen(3000)
 
+
+
+var url = require('url')
+
+http.createServer(function(req,res){
+       res.writeHead(200,{'Content-type':'text/html'})
+       var q = url.parse(req.url,true).query
+       var text = q.month + " " + q.year
+       res.write(text)
+       res.end()
+}).listen(3000) */
