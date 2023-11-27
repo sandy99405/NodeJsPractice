83beb62b3e388caada4d17eb9ee55d1a9c5efbe9
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2023-11-27 12:19:55 +0530

    File system done

--- urlPractice.js
+++ urlPractice.js
@@ -19,3 +19,25 @@ http.createServer(function(req,res){
               res.end()
 }).listen(3000)
 
+
+
+var url = require('url')
+
+http.createServer(function(req,res){
+              res.writeHead(200,{'Content-type':'text/html'})
+              var q = url.parse(req.url,true).query
+              var text = q.month + " " + q.year
+              res.write(text)
+              res.end()
+}).listen(3000)
+
+
+var fs = require('fs')
+
+fs.writeFile("DemoFile.html",function(err,data){
+              http.createFile(function(req,res){
+                    res.writeHead(200,{'Content-type':'text/html'})
+                    res.write(data)
+                    return res.end()
+              }).listen(3000)
+})
