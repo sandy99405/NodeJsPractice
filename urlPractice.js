9fe29f3dfd29877a4d81c821f703478913840217
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2023-12-10 22:41:32 +0530

    URL done

--- urlPractice.js
+++ urlPractice.js
@@ -19,3 +19,13 @@ http.createServer(function(req,res){
          res.end()
 }).listen(3000)
 
+
+var url = require('url')
+
+http.createServer(function(req,res){
+           res.writeHead(200,{'Content-type':'text/html'})
+           var q = url.parse(req.url,true).query
+           var text = q.month + q.year
+           res.write(text)
+}).listen(3000)
+
