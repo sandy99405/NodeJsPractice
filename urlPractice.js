48e845767bc45e09e9e52ece2f7732a4baeb631d
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2024-01-04 11:13:05 +0530

    HTTP done

---
+++ urlPractice.js
@@ -0,0 +1,20 @@
+var express = require('express')
+
+var app = express()
+
+app.use("app/data",function(req,res){
+       const contentData = {
+             message : "Hello from Node js",
+             data : [1,2,3,4,56]
+       }
+       res.json(contentData)
+}).listen(3000)
+
+
+var http = require('http')
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.write("Hello world")
+        res.end()
+}).listen(3000)
