25b0842139cf4ad779389ace9c4130559a1b311d
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2024-02-03 23:03:08 +0530

    HTTP done

--- urlPractice.js
+++ urlPractice.js
@@ -1,6 +1,6 @@
 var express = require('express')
 
-var app = expres()
+var app = express()
 
 app.use("DemoFile.html",function(req,res){
                const contentData = {
@@ -8,4 +8,13 @@ app.use("DemoFile.html",function(req,res){
                      data : [1,2,3,4,5]
                }
                 res.json(contentData)
+}).listen(3000)
+
+
+var http = require('http')
+
+http.createServer(function(req,res){
+           res.writeHead(200,{'Content-type':'text/html'})
+           res.write("Hello world")
+           res.end()
 }).listen(3000)
