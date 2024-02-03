d284a008ca0c368c5c8131d1024e8202ba0ffee1
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2024-02-03 23:24:52 +0530

    File system done

--- urlPractice.js
+++ urlPractice.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var app = express()
 
-app.use("DemoFile.html",function(req,res){
+app.use("app/data",function(req,res){
                const contentData = {
                      message : "Hello from Node js",
                      data : [1,2,3,4,5]
@@ -29,3 +29,14 @@ http.createServer(function(req,res){
          res.write(text)
          res.end()
 }).listen(3000)
+
+
+var fs = require('fs')
+
+fs.writeFile("DemoFile.html",function(err,data){
+        http.createServer(function(req,res){
+             res.writeHead(200,{'Content-type':'text/html'})
+             res.write(data)
+             res.end()
+        }).listen(3000)
+})
