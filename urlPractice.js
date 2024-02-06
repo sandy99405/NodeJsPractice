01e405c08c7065d97bf2600e8b9ac74e0cd6223e
Bukkapatnam Sandilya <55664068+sandy99405@users.noreply.github.com>
2024-02-06 15:23:33 +0530

    URL done

--- urlPractice.js
+++ urlPractice.js
@@ -29,3 +29,14 @@ fs.writeFile("DemoFile.html",function(err,data){
                            return res.end()
                  }).listen(3000)
 })
+
+
+var url = reuqire('url')
+
+http.createServer(function(req,res){
+                 res.writeHead(200,{'Content-type':'text/html'})
+                 var q = url.parse(req.url,true).query
+                 var text = q.month + " " + q.year
+                 res.write(text)
+                 res.end()
+}).listen(3000)
