84fb06be5c137a6f833ef7821b2804111271e9fc
Bukkapatnam Sandilya <sandysandilya99@gmail.com>
2024-02-12 22:11:45 +0530

    File system done

--- urlPractice.js
+++ urlPractice.js
@@ -28,3 +28,14 @@ http.createServer(function(req,res){
             res.write(text)
             res.end()
 }).listen(3000)
+
+
+var fs = require('fs')
+
+fs.writeFile(function(err,data){
+         http.createServer(function(req,res){
+                res.writeHead(200,{'Content-type':'text/html'})
+                res.write(data)
+                res.end()
+         }).listen(3000)
+})
